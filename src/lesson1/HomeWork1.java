6d2304326faa4ff4efe6afec36b14b9b052f7122
hakingdev <j72Y7c62Nm@2fQJ>
2023-09-20 14:01:53 +0200

    first_step_commit

---
+++ src/lesson1/HomeWork1.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class HomeWork1 {
+    public static void sayHello() {
+        String hello = "Hi";
+        System.out.println(hello);
+
+    }
+
+    public static void main(String[] args) {
+
+        sayHello();
+    }
+
+}
